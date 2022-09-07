8c962e4642d193961a6d31c272a6ca343f0a957d
David Goffredo <david.goffredo@datadoghq.com>
2022-09-07 19:07:15 -0400

    derp

--- program.cpp
+++ program.cpp
@@ -1,7 +1,6 @@
 // Based off of:
 // <https://github.com/nginx/nginx/blob/a64190933e06758d50eea926e6a55974645096fd/src/os/unix/ngx_daemon.c>
 
-#include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -36,7 +35,6 @@ int main() {
 }
 
 void before_fork() {
-    std::ifstream stream{"/etc/file"};
     if (std::ifstream(file)) {
         std::cerr << "I am the parent before fork.  The file stream is good." << std::endl;
     } else {
@@ -46,7 +44,6 @@ void before_fork() {
 
 
 void after_fork_parent() {
-    std::ifstream stream{"/etc/file"};
     if (std::ifstream(file)) {
         std::cerr << "I am the parent after fork.  The file stream is good." << std::endl;
     } else {
@@ -62,7 +59,6 @@ void after_fork_child() {
 
     umask(0);
 
-    std::ifstream stream{"/etc/file"};
     if (std::ifstream(file)) {
         std::cerr << "I am the child after fork.  The file stream is good." << std::endl;
     } else {
